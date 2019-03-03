e7eb0537f181b6ae00eda8db8dd79ee596665f63
syntaxhacker <30970495+syntaxhacker@users.noreply.github.com>
2019-03-03 22:23:41 +0530

    first week finished

---
+++ algorithmic-toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+
+
+using  namespace std;
+
+long long int MaxPairwiseProductFast(vector<int> arr, int n) {
+	sort(arr.begin(), arr.end()); 
+	return (long long int)(arr[n - 1]) * (arr[n - 2]); 
+}
+
+int main() {
+	int n; 
+	cin >> n; 
+	vector<int> arr(n);
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i]; 
+	}
+	long long int Res = MaxPairwiseProductFast(arr, n);
+	cout << Res << endl; 
+}
